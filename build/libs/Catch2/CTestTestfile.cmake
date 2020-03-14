d5ba3180e5cb13224ab3fc01b0b363ec6cb7cf74
Yosua Martiansia <yosuamartiansia@gmail.com>
2020-03-14 10:04:56 +0700

    Removed build

--- build/libs/Catch2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yosua/Projects/Shapes2D/Shapes2D-CPP/libs/Catch2
-# Build directory: /home/yosua/Projects/Shapes2D/Shapes2D-CPP/build/libs/Catch2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
